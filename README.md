a66d7fc9dfd0e4dea6a294b0dc802d31d2909b99
cornel creanga <cornelcreanga@yahoo.com>
2017-01-29 12:04:24 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Offers number spellout support (for the moment only for EN/RO language)
+
+The spelling for romanian language is more 'natural' than the one provided by ICU4J.
+
+Eg:
+
+ICU4J
+
+una sută douăzeci şi şapte miliarde şase sute optzeci şi şapte milioane cinci sute cincizeci şi şapte mii opt sute şaptezeci şi trei virgulă şase şapte
+
+This library
+
+**o** sută douăzeci și șapte **de** miliarde șase sute optzeci și șapte de milioane cinci sute cincizeci și șapte **de** mii opt sute șaptezeci și trei virgulă **șaizeci și șapte**
--- src/main/java/com/ntw/ICUTest.java
+++ src/main/java/com/ntw/ICUTest.java
@@ -8,9 +8,9 @@ import java.util.Locale;
 public class ICUTest {
     public static void main(String[] args) {
         RuleBasedNumberFormat ruleBasedNumberFormat = new RuleBasedNumberFormat( new Locale("RO", "RO"), RuleBasedNumberFormat.SPELLOUT );
-        System.out.println(ruleBasedNumberFormat.format(127688687557873.67));
+        System.out.println(ruleBasedNumberFormat.format(127687557873.67));
         NumberToWords ntw = NumberToWordsFactory.getConvertor(Locale.forLanguageTag("ro-RO"));
-        System.out.println(ntw.convert(new BigDecimal(127688687557873.67)));
+        System.out.println(ntw.convert(new BigDecimal(127687557873.67)));
     }
 
 }
